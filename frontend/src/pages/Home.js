4951ef084846c31d5b8e0d39b8767b081a7da45c
Anitta Antony <anittaantonyal>
2025-04-29 20:50:22 +0200

    home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,12 @@
-// src/pages/Home.js
 import React from 'react';
 import '../styles/Home.css';
+import bgImage from '../assets/Hero-bg.png'; // adjust path if needed
 
 function Home() {
   return (
-    <div className="home-container">
+    <div className="hero" style={{ backgroundImage: `url(${bgImage})` }}>
       <h1>Welcome to Weave Haven</h1>
       <p>Your one-stop shop for beautiful fashion!</p>
-      {/* Buttons removed in favor of navbar navigation */}
     </div>
   );
 }
