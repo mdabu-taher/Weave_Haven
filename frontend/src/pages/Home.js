4c03d56aba93d367f4923e41a8b9d4e2285b3691
Md Abu Taher <159029567+mdabu-taher@users.noreply.github.com>
2025-05-15 10:20:12 +0200

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ import slide2 from '../assets/slide-2.jpg';
 import slide3 from '../assets/slide-3.jpg';
 import slide4 from '../assets/slide-4.jpg';
 import slide5 from '../assets/slide-5.JPG';
-import slide6 from '../assets/slide-6.JPG';
+import slide6 from '../assets/slide-6.jpg';
 
 export default function Home() {
   // track whether we’re on a narrow screen
