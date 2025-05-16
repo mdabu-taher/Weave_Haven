4f22b538d1fe58fac092b60588e7cc2f8cbafdfb
AnittaAntony11 <159579925+AnittaAntony11@users.noreply.github.com>
2025-05-16 20:51:21 +0200

    Update Product.js

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema({
     required: true,
     min: 0
   },
-  // optional sale price, must be less than regular price if set
+  // optional sale price must be less than regular price if set
   salePrice: {
     type: Number,
     min: 0,
