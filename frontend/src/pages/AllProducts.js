2eb9bba5308e75e3e5d8c48851d40599690f9748
AnittaAntony11 <159579925+AnittaAntony11@users.noreply.github.com>
2025-05-16 20:40:34 +0200

    Update AllProducts.js

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -15,7 +15,7 @@ export default function AllProducts() {
         let filtered = data;
 
         if (categoryFromPath === 'new-arrivals') {
-          // Show only products created in the last 14 days
+          // Show only products created in the last 14 day
           const TWO_WEEKS_MS = 14 * 24 * 60 * 60 * 1000;
           const cutoff = Date.now() - TWO_WEEKS_MS;
           filtered = data.filter(
